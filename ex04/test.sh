d1732e58b0b52146fcf08730b3f579c90dff8574
mfunyu <mfunyu@student.42tokyo.jp>
2021-11-11 14:43:54 +0900

    ex04 [tester] test.sh test/

---
+++ ex04/test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# define
+THICK="\033[1m"
+CYAN="\033[1;36m"
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+RESET="\033[m"
+PROMPT="${CYAN}$>${RESET}"
+
+REPLACE="./replace"
+TESTFILEDIR="test"
+
+FILES=`ls -1 ${TESTFILEDIR}`
+FLIELST=(${FILES// / })
+
+
+function test() {
+	printf "$CYAN[ s1 = \"$1\", s2 = \"$2\" ]\n$RESET"
+	for file in "${FLIELST[@]}"
+	do
+		printf "${PROMPT} ${REPLACE} $TESTFILEDIR/$file $1 $2\n${RESET}"
+		$REPLACE $TESTFILEDIR/$file $1 $2
+	done
+	printf "\n"
+}
+
+test $1 $2
+
+ls -l $TESTFILEDIR
